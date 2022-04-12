6b2d6ac0bf24ad5bc4ace13e4a028f1c09a98a01
XerolySkinner <63062392+XerolySkinner@users.noreply.github.com>
2022-04-13 01:02:47 +0800

    元始版本

---
+++ System_Core/BasicDefine.h
@@ -0,0 +1,98 @@
+/*----------------------------------------------------------------------------------------------------
+#	THIS FILE IS PART OF XEROLYSKINNER'S PROJECT
+#
+#	THIS PROGRAM IS FREE SOFTWARE
+#
+#	E-mail:ZABBCCCBBAZ@163.com
+#	QQ:2715099320
+#
+#	Copyright (c) 2022 XerolySkinner
+#	All rights reserved.
+*/
+
+/*----------------------------------------------------------------------------------------------------
+	名字:BasicDefine
+	时间:20220413-0036
+	程序员:梁源康
+	说明:基础宏定义
+*/
+#ifndef _XEROLYSKINNER_BASICDEFINE_H
+#define _XEROLYSKINNER_BASICDEFINE_H
+#include "vartable.h"
+#include "stdint.h"
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------------------------------
+//	常用类型定义
+typedef						int32_t		s32;
+typedef						int16_t		s16;
+typedef						int8_t		s8;
+typedef						uint32_t	u32;
+typedef						uint16_t	u16;
+typedef						uint8_t		u8;
+
+typedef const				int32_t		sc32;
+typedef const				int16_t		sc16;
+typedef const				int8_t		sc8;
+typedef const				uint32_t	uc32;  
+typedef const				uint16_t	uc16;
+typedef const				uint8_t		uc8;
+
+typedef volatile			int32_t		vs32;
+typedef	volatile 			int16_t		vs16;
+typedef	volatile 			int8_t		vs8;
+typedef	volatile			uint32_t	vu32;
+typedef volatile			uint16_t	vu16;
+typedef volatile			uint8_t		vu8;
+
+typedef	volatile const		int32_t		vsc32;
+typedef	volatile const		int16_t		vsc16;
+typedef	volatile const		int8_t		vsc8;
+typedef volatile const		uint32_t	vuc32;
+typedef volatile const		uint16_t	vuc16;
+typedef volatile const		uint8_t		vuc8;
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//----------------------------------------------------------------------------------------------------
+//	位带操作
+#define BITBAND(addr, bitnum) ((addr & 0xF0000000)+0x2000000+((addr &0xFFFFF)<<5)+(bitnum<<2)) 
+#define MEM_ADDR(addr)  *((volatile unsigned long  *)(addr)) 
+#define BIT_ADDR(addr, bitnum)   MEM_ADDR(BITBAND(addr, bitnum)) 
+//	IO口地址映射
+#define GPIOA_ODR_Addr    (GPIOA_BASE+12) //0x4001080C 
+#define GPIOB_ODR_Addr    (GPIOB_BASE+12) //0x40010C0C 
+#define GPIOC_ODR_Addr    (GPIOC_BASE+12) //0x4001100C 
+#define GPIOD_ODR_Addr    (GPIOD_BASE+12) //0x4001140C 
+#define GPIOE_ODR_Addr    (GPIOE_BASE+12) //0x4001180C 
+#define GPIOF_ODR_Addr    (GPIOF_BASE+12) //0x40011A0C    
+#define GPIOG_ODR_Addr    (GPIOG_BASE+12) //0x40011E0C    
+
+#define GPIOA_IDR_Addr    (GPIOA_BASE+8) //0x40010808 
+#define GPIOB_IDR_Addr    (GPIOB_BASE+8) //0x40010C08 
+#define GPIOC_IDR_Addr    (GPIOC_BASE+8) //0x40011008 
+#define GPIOD_IDR_Addr    (GPIOD_BASE+8) //0x40011408 
+#define GPIOE_IDR_Addr    (GPIOE_BASE+8) //0x40011808 
+#define GPIOF_IDR_Addr    (GPIOF_BASE+8) //0x40011A08 
+#define GPIOG_IDR_Addr    (GPIOG_BASE+8) //0x40011E08 
+ 
+//	IO口操作,只对单一的IO口
+#define PAout(n)   BIT_ADDR(GPIOA_ODR_Addr,n)  //输出 
+#define PAin(n)    BIT_ADDR(GPIOA_IDR_Addr,n)  //输入 
+
+#define PBout(n)   BIT_ADDR(GPIOB_ODR_Addr,n)  //输出 
+#define PBin(n)    BIT_ADDR(GPIOB_IDR_Addr,n)  //输入 
+
+#define PCout(n)   BIT_ADDR(GPIOC_ODR_Addr,n)  //输出 
+#define PCin(n)    BIT_ADDR(GPIOC_IDR_Addr,n)  //输入 
+
+#define PDout(n)   BIT_ADDR(GPIOD_ODR_Addr,n)  //输出 
+#define PDin(n)    BIT_ADDR(GPIOD_IDR_Addr,n)  //输入 
+
+#define PEout(n)   BIT_ADDR(GPIOE_ODR_Addr,n)  //输出 
+#define PEin(n)    BIT_ADDR(GPIOE_IDR_Addr,n)  //输入
+
+#define PFout(n)   BIT_ADDR(GPIOF_ODR_Addr,n)  //输出 
+#define PFin(n)    BIT_ADDR(GPIOF_IDR_Addr,n)  //输入
+
+#define PGout(n)   BIT_ADDR(GPIOG_ODR_Addr,n)  //输出 
+#define PGin(n)    BIT_ADDR(GPIOG_IDR_Addr,n)  //输入
+
+#endif
